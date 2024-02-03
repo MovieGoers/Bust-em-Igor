2cb0e713e95761f679cd1d226aa7a440da1aec4a
MovieGoers <alscjf4104@gmail.com>
2024-02-03 21:17:09 +0900

    added : 플레이어 스크립트 추가.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float damage;
+    public float speed;
+    public float hp;
+    public float attackRate;
+
+}
