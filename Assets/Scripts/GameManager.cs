82144e88ac852b0fb54eae9a71f8d9c07fe32361
MovieGoers <alscjf4104@gmail.com>
2024-02-03 21:06:10 +0900

    added : 게임 매니저 추가.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
