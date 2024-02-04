067dac73500b5de7a09c2c463ba18976af6707ac
MovieGoers <alscjf4104@gmail.com>
2024-02-04 14:44:16 +0900

    hp, attack 관련 UI 함수 추가.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private static UIManager instance;
+
+    public Text hpText;
+    public Text AttackText;
+    public static UIManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void SetHPText(float hp)
+    {
+        hpText.text = "HP : " + hp;
+    }
+
+    public void SetAttackText(float time)
+    {
+        AttackText.text = "Attack : " + time;
+    }
 }
